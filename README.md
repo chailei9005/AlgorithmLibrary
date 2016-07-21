5803646957584d565479f898030ba0eaef0ec2cb
ChuyangLiu <512045973@qq.com>
2016-07-21 18:14:00 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# DataStructureAndAlgorithm
+
+A library that contains useful data structures and algorithms.
+
+## Compile and Run
+
+```bash
+$ cd src
+$ make
+$ make run
+$ make clean
+```
+
+## License
+
+Copyright 2016 ChuyangLiu
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+// #include "myList.h"
+// #include "myStack.h"
+// #include "myQueue.h"
+// #include "myAvlTree.h"
+// #include "myHashTable.h"
+// #include "myBinaryHeap.h"
+// #include "myLeftistHeap.h"
+// #include "myBinomialQueue.h"
+// #include "myDisjointSet.h"
+// #include "myGraph.h"
+
+// #include "myAlgorithm.h"
+// #include "myRandom.h"
+// #include "mySortHelper.h"
+
+// #include "ArithmeticExpression.h"
+// #include "NumberSystem.h"
+// #include "BigInteger.h"
+
+void test() {
+    //testList();
+    //testStack();
+    //testQueue();
+    //testAvlTree();
+    //testHashTable();
+    //testBinaryHeap();
+    //testLeftistHeap();
+    //testBinomialQueue();
+    //testDisjointSets();
+    //testGraph();
+
+    //testMyAlgorithm();
+    //testMyRandom();
+    //testSort();
+
+    //testArithmeticExpre();
+    //testNumberSystem();
+    //testBigInteger();
+}
+
+int main() {
+    test();
+    return 0;
+}
